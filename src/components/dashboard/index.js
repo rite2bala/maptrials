8af0a07fa0088b66e15a45140992150884ae3eed
rite2bala <44022980+rite2bala@users.noreply.github.com>
2019-08-15 22:46:16 +0530

    commit

--- src/components/dashboard/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-//import DenseTable from "./datatable";
-//import TextFields from "./form";
-
-import Dashboard from "./dbscenes/cards";
-
-export default props => <Dashboard />;
